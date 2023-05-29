29517be4587ea96c58b8b0ce93bff48014230a4a
Alan Gabriel Dueñas Nuñez <gabrie1_4401@hotmail.com>
2023-05-29 15:25:53 -0700

    update to edit and delete

--- src/components/StatusPicker.jsx
+++ src/components/StatusPicker.jsx
@@ -5,27 +5,48 @@ import { faHourglass } from "@fortawesome/free-solid-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
-export function StatusPicker({ statusPicked }) {
+export function StatusPicker({ statusPicked, _id }) {
   statusPicked = statusPicked.toLowerCase().replace(" ", "");
+
+  function handleStatusChange(_id) {
+    //where _id == to db id change status
+    console.log("changed status");
+  }
+
   return (
     <div className="status-dropdown">
-      <button className={`btn btn-small btn-fixed-width ${statusPicked === "citado" && statusPicked}`}>
+      <button
+        className={`btn btn-small btn-fixed-width ${statusPicked === "citado" && statusPicked}`}
+        onClick={() => handleStatusChange(_id)}
+      >
         <FontAwesomeIcon className="icon-secundary" icon={faCalendar} />
         <p className="small-font">Citado</p>
       </button>
-      <button className={`btn btn-small btn-fixed-width ${statusPicked === "pendiente" && statusPicked}`}>
+      <button
+        className={`btn btn-small btn-fixed-width ${statusPicked === "pendiente" && statusPicked}`}
+        onClick={() => handleStatusChange(_id)}
+      >
         <FontAwesomeIcon className="icon-secundary" icon={faClock} />
         <p className="small-font">Pendiente</p>
       </button>
-      <button className={`btn btn-small btn-fixed-width ${statusPicked === "encurso" && statusPicked}`}>
+      <button
+        className={`btn btn-small btn-fixed-width ${statusPicked === "encurso" && statusPicked}`}
+        onClick={() => handleStatusChange(_id)}
+      >
         <FontAwesomeIcon className="icon-secundary" icon={faHourglass} />
         <p className="small-font">En Curso</p>
       </button>
-      <button className={`btn btn-small btn-fixed-width ${statusPicked === "completado" && statusPicked}`}>
+      <button
+        className={`btn btn-small btn-fixed-width ${statusPicked === "completado" && statusPicked}`}
+        onClick={() => handleStatusChange(_id)}
+      >
         <FontAwesomeIcon className="icon-secundary" icon={faCheck} />
         <p className="small-font">Completado</p>
       </button>
-      <button className={`btn btn-small btn-fixed-width ${statusPicked === "cancelado" && statusPicked}`}>
+      <button
+        className={`btn btn-small btn-fixed-width ${statusPicked === "cancelado" && statusPicked}`}
+        onClick={() => handleStatusChange(_id)}
+      >
         <FontAwesomeIcon className="icon-secundary" icon={faXmark} />
         <p className="small-font">Cancelado</p>
       </button>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -13,6 +13,7 @@ import { faClock } from "@fortawesome/free-solid-svg-icons";
 import { faHourglass } from "@fortawesome/free-solid-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
 import { useForm } from "react-hook-form";
 import { FormDatePicker } from "./FormDatePicker";
@@ -20,13 +21,7 @@ import { FormTimePicker } from "./FormTimePicker";
 import dayjs from "dayjs";
 import updateLocale from "dayjs/plugin/updateLocale";
 import { useRouter } from "next/router";
-import {
-  getWeek,
-  getMonth,
-  filterByDateOrRange,
-  resetTimeOnDate,
-  dateExistsInSelected,
-} from "../utils/utils";
+import { getWeek, getMonth, filterByDateOrRange, resetTimeOnDate, dateExistsInSelected } from "../utils/utils";
 
 import convertToDateType from "../utils/utils";
 import { Dropdown } from "antd";
@@ -40,11 +35,10 @@ import { useState, useEffect } from "react";
 export default function Table() {
   const [appointments, setAppointments] = useState([]);
   const [modalShown, setModalShown] = useState(false);
-  const [selectedDateOrRange, setSelectedDateOrRange] = useState([
-    resetTimeOnDate(new Date()),
-  ]);
+  const [selectedDateOrRange, setSelectedDateOrRange] = useState([resetTimeOnDate(new Date())]);
   const [search, setSearch] = useState("");
-  const { register, handleSubmit, reset, control } = useForm();
+  const { register, handleSubmit, reset, control, setValue } = useForm();
+  const [modalType, setModalType] = useState("Crear");
   const { reload } = useRouter();
   dayjs.extend(updateLocale);
   dayjs.updateLocale("en", {
@@ -74,9 +68,6 @@ export default function Table() {
   async function addNewAppointment(inputs) {
     if (inputs.servicio === "Servicio") inputs.servicio = "Corte de cabello";
 
-    //ingresar en base de datos
-
-    //Si existe no crea ni vergas
     inputs.fecha = dayjs(inputs.fecha).format("DD-MM-YYYY");
     inputs.hora = dayjs(inputs.hora).format("h:mm a");
     inputs.estado = "Citado";
@@ -103,9 +94,82 @@ export default function Table() {
     setModalShown(false);
   }
 
+  async function updateAppointment(inputs) {
+    //base de datos
+
+    if (!(inputs.fecha instanceof Date)) inputs.fecha = inputs.fecha.toDate();
+    inputs.fecha = resetTimeOnDate(inputs.fecha);
+
+    inputs.fecha = dayjs(inputs.fecha).format("DD-MM-YYYY");
+    inputs.hora = dayjs(inputs.hora).format("h:mm a");
+
+    const newAppointments = appointments.map(item => {
+      if (item._id === inputs._id) {
+        return {
+          _id: inputs._id,
+          fecha: inputs.fecha,
+          hora: inputs.hora,
+          nombre: inputs.nombre,
+          servicio: inputs.servicio,
+          telefono: inputs.telefono,
+          estado: inputs.estado,
+        };
+      }
+      return item;
+    });
+
+    // !quitar
+    setAppointments(newAppointments);
+
+    reset({
+      fecha: "",
+      hora: "",
+      nombre: "",
+      servicio: "Servicio",
+      telefono: "",
+      _id: "",
+      estado: "",
+    });
+
+    setModalShown(false);
+  }
+
+  function handleAddModalOpening() {
+    setModalType("Crear");
+    setModalShown(true);
+
+    //reset inputs
+    setValue("nombre", "");
+    setValue("telefono", "");
+    setValue("servicio", "Servicio");
+    setValue("fecha", null);
+    setValue("hora", null);
+    setValue("_id", "");
+    setValue("estado", "");
+  }
+
+  function handleEditModalOpening({ _id, nombre, telefono, servicio, hora, fecha, estado }) {
+    setModalType("Actualizar");
+    setModalShown(true);
+
+    //Populate date and time inputs on form
+    const auxFecha = fecha.split("-");
+    fecha = new Date(+auxFecha[2], auxFecha[1] - 1, +auxFecha[0]);
+    const convertTo24 = e => (e ? dayjs(`1/1/1 ${e}`).format("HH:mm:00") : null);
+    hora = convertTo24(hora);
+    hora = hora.split(":");
+    hora = new Date(fecha.getFullYear(), fecha.getMonth(), fecha.getDate(), hora[0], hora[1], hora[2]);
+
+    setValue("nombre", nombre);
+    setValue("telefono", telefono);
+    setValue("servicio", servicio);
+    setValue("fecha", fecha);
+    setValue("hora", hora);
+    setValue("_id", _id);
+    setValue("estado", estado);
+  }
+
   function handleDateChange(inputDate, type) {
-    //!cambiar onchange de select date picker para que actualize tambien
-    //Resetea la hora del Date a 00:00:00
     inputDate = resetTimeOnDate(inputDate.toDate());
 
     if (type === "date") {
@@ -129,26 +193,29 @@ export default function Table() {
       const { start, end } = getMonth(selectedDateOrRange[0]);
       setSelectedDateOrRange([start, end]);
     } else if (picker === "todas") {
-      setSelectedDateOrRange([
-        new Date("January 01, 2023"),
-        resetTimeOnDate(new Date()),
-      ]);
+      setSelectedDateOrRange([new Date("January 01, 2023"), new Date("December 31, 2023")]);
     }
+  }
+
+  async function handleDelete({ _id }) {
+    // try {
+    //   await fetch("http://localhost:3000/api/appoint", {
+    //     method: "DELETE",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(id),
+    //   });
+    //   console.log(inputs);
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }
 
   function selectPreviousDay() {
     if (selectedDateOrRange.length === 1) {
-      setSelectedDateOrRange((prev) => [
-        new Date(prev[0].getTime() - 86400000),
-      ]);
+      setSelectedDateOrRange(prev => [new Date(prev[0].getTime() - 86400000)]);
     } else if (selectedDateOrRange.length === 2) {
-      if (
-        selectedDateOrRange[1].getTime() - selectedDateOrRange[0].getTime() <=
-        604800000
-      ) {
-        const { start, end } = getWeek(
-          new Date(selectedDateOrRange[0].getTime() - 604800000)
-        ); //get previous week range
+      if (selectedDateOrRange[1].getTime() - selectedDateOrRange[0].getTime() <= 604800000) {
+        const { start, end } = getWeek(new Date(selectedDateOrRange[0].getTime() - 604800000)); //get previous week range
         setSelectedDateOrRange([start, end]);
       } else {
         let previousMonth = selectedDateOrRange[0];
@@ -163,17 +230,10 @@ export default function Table() {
 
   function selectNextDay() {
     if (selectedDateOrRange.length === 1) {
-      setSelectedDateOrRange((prev) => [
-        new Date(prev[0].getTime() + 86400000),
-      ]);
+      setSelectedDateOrRange(prev => [new Date(prev[0].getTime() + 86400000)]);
     } else if (selectedDateOrRange.length === 2) {
-      if (
-        selectedDateOrRange[1].getTime() - selectedDateOrRange[0].getTime() <=
-        604800000
-      ) {
-        const { start, end } = getWeek(
-          new Date(selectedDateOrRange[0].getTime() + 604800000)
-        ); //get next week range
+      if (selectedDateOrRange[1].getTime() - selectedDateOrRange[0].getTime() <= 604800000) {
+        const { start, end } = getWeek(new Date(selectedDateOrRange[0].getTime() + 604800000)); //get next week range
         setSelectedDateOrRange([start, end]);
       } else {
         let previousMonth = selectedDateOrRange[0];
@@ -186,36 +246,14 @@ export default function Table() {
     }
   }
 
-  async function handleDelete(id) {
-    try {
-      await fetch("http://localhost:3000/api/appoint", {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(id),
-      });
-      console.log(inputs);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   return (
     <>
       <div className="flex">
         <div className="searchbar flex no-gap">
           <button className="btn">
-            <FontAwesomeIcon
-              className="icon-primary"
-              icon={faMagnifyingGlass}
-            />
+            <FontAwesomeIcon className="icon-primary" icon={faMagnifyingGlass} />
           </button>
-          <input
-            value={search}
-            className="input-accent"
-            type="text"
-            placeholder="Buscar"
-            onChange={(e) => setSearch(e.target.value)}
-          />
+          <input value={search} className="input-accent" type="text" placeholder="Buscar" onChange={e => setSearch(e.target.value)} />
         </div>
         <div className="flex small-gap">
           <button className="btn" onClick={selectPreviousDay}>
@@ -233,14 +271,10 @@ export default function Table() {
               />
             )}
           >
-            <div
-              className="btn"
-              style={{ width: "16.5rem", justifyContent: "space-between" }}
-            >
+            <div className="btn" style={{ width: "16.5rem", justifyContent: "space-between" }}>
               <p className="accent-font">
                 {dayjs(selectedDateOrRange[0]).format("DD-MM-YYYY")}
-                {selectedDateOrRange.length === 2 &&
-                  ` | ${dayjs(selectedDateOrRange[1]).format("DD-MM-YYYY")}`}
+                {selectedDateOrRange.length === 2 && ` | ${dayjs(selectedDateOrRange[1]).format("DD-MM-YYYY")}`}
               </p>
               <FontAwesomeIcon className="icon-primary" icon={faChevronDown} />
             </div>
@@ -249,35 +283,38 @@ export default function Table() {
             <FontAwesomeIcon className="icon-primary" icon={faChevronRight} />
           </button>
         </div>
-        <button className="btn" onClick={() => setModalShown(true)}>
+        <button className="btn" onClick={handleAddModalOpening}>
           <FontAwesomeIcon className="icon-primary" icon={faPlus} />
           <p className="accent-font">Nueva Cita</p>
         </button>
       </div>
       <div className="table flex-column">
         <div className="table-item table-head">
-          <div>
-            <button className="btn btn-small">
-              <FontAwesomeIcon className="icon-primary" icon={faSquare} />
-            </button>
-          </div>
           <h3>Fecha</h3>
           <h3>Hora</h3>
           <h3>Nombre</h3>
           <h3>Servicio</h3>
           <h3>Telefono</h3>
           <h3>Estado</h3>
-          <div>
-            <button className="btn btn-small">
-              <FontAwesomeIcon className="icon-primary" icon={faSort} />
-            </button>
-          </div>
+          <div></div>
+          <button className="btn btn-small">
+            <FontAwesomeIcon className="icon-primary" icon={faSort} />
+          </button>
         </div>
 
         {appointments
-          .filter((i) => convertToDateType(i.fecha) >= selectedDateOrRange[0])
+          .filter(i => {
+            if (selectedDateOrRange.length === 1) {
+              return convertToDateType(i.fecha).getTime() === selectedDateOrRange[0].getTime();
+            } else if (selectedDateOrRange.length === 2) {
+              return (
+                convertToDateType(i.fecha).getTime() >= selectedDateOrRange[0].getTime() &&
+                convertToDateType(i.fecha).getTime() <= selectedDateOrRange[1].getTime()
+              );
+            }
+          })
           .filter(
-            (e) =>
+            e =>
               e.nombre.toLowerCase().includes(search) ||
               e.telefono.includes(search) ||
               e.servicio.toLowerCase().includes(search) ||
@@ -285,17 +322,9 @@ export default function Table() {
               e.fecha.toLowerCase().includes(search) ||
               e.hora.toLowerCase().includes(search)
           )
-          .map((e) => {
+          .map(e => {
             return (
               <div key={e._id} className="table-item table-row">
-                <div>
-                  <button className="btn btn-small">
-                    <FontAwesomeIcon
-                      className="icon-secundary"
-                      icon={faSquare}
-                    />
-                  </button>
-                </div>
                 <p className="small-font">{e.fecha}</p>
                 <p className="small-font">{e.hora}</p>
                 <p className="small-font">{e.nombre}</p>
@@ -305,9 +334,7 @@ export default function Table() {
                   className="btn btn-small btn-fixed-width"
                   trigger={["click"]}
                   placement="bottom"
-                  dropdownRender={() => (
-                    <StatusPicker statusPicked={e.estado} />
-                  )}
+                  dropdownRender={() => <StatusPicker statusPicked={e.estado} _id={e._id} />}
                 >
                   <div>
                     <FontAwesomeIcon
@@ -323,14 +350,12 @@ export default function Table() {
                     <p className="small-font">{e.estado}</p>
                   </div>
                 </Dropdown>
-                <div>
-                  <button className="btn btn-small">
-                    <FontAwesomeIcon
-                      className="icon-secundary"
-                      icon={faTrash}
-                    />
-                  </button>
-                </div>
+                <button className="btn btn-small">
+                  <FontAwesomeIcon className="icon-secundary" onClick={() => handleEditModalOpening(e)} icon={faPenToSquare} />
+                </button>
+                <button className="btn btn-small">
+                  <FontAwesomeIcon className="icon-secundary" onClick={() => handleDelete(e)} icon={faTrash} />
+                </button>
               </div>
             );
           })}
@@ -345,28 +370,22 @@ export default function Table() {
 
       {modalShown && (
         <div className="modal-container">
-          <form
-            onSubmit={handleSubmit(addNewAppointment)}
-            className="modal flex-column"
-          >
-            <h1 style={{ marginBottom: "1rem" }}>Crear cita</h1>
+          <form onSubmit={handleSubmit(modalType === "Crear" ? addNewAppointment : updateAppointment)} className="modal flex-column">
+            <h1 style={{ marginBottom: "1rem" }}>{modalType} cita</h1>
             <div className="flex" style={{ alignItems: "flex-start" }}>
               <div className="flex-column column-gap">
-                <input
-                  placeholder="Nombre"
-                  {...register("nombre", { required: true, maxLength: 50 })}
-                />
-                <input
-                  placeholder="Telefono"
-                  {...register("telefono", { required: true, maxLength: 50 })}
-                />
+                <input type="text" {...register("_id")} disabled style={{ display: "none" }} />
+                <input type="text" {...register("estado")} disabled style={{ display: "none" }} />
+                <input placeholder="Nombre" {...register("nombre", { required: true, maxLength: 50 })} />
+                <input placeholder="Telefono" {...register("telefono", { required: true, maxLength: 50 })} />
                 <select {...register("servicio")} defaultValue="Servicio">
                   <option value="Servicio" disabled hidden>
                     Servicio
                   </option>
                   <option value="Corte de cabello">Corte de cabello</option>
-                  <option value="Tinte">Tinte</option>
+                  <option value="Pintura de cabello">Pintura de cabello</option>
                   <option value="Maquillaje">Maquillaje</option>
+                  <option value="Tinte">Tinte</option>
                 </select>
               </div>
               <div className="flex-column column-gap">
@@ -375,14 +394,11 @@ export default function Table() {
               </div>
             </div>
             <div className="flex align-right align-bottom">
-              <button
-                className="btn btn-secundary"
-                onClick={() => setModalShown(false)}
-              >
+              <button className="btn btn-secundary" onClick={() => setModalShown(false)}>
                 <p>Cancelar</p>
               </button>
               <button className="btn" type="submit">
-                <p className="accent-font">Crear</p>
+                <p className="accent-font">{modalType}</p>
               </button>
             </div>
           </form>
